0313bbd0ca393e440edf34f429255508116041f8
Enrique García <kikito@gmail.com>
2011-11-27 16:28:25 +0100

    initial version

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title>Hosting y Dominios - VirtualHostingDigital</title>
+<meta http-equiv="title" content="virtual hosting digital" />
+<meta name="keywords" content="hosting, hosting barato, hosting economico, dominio, dominios, dominios baratos, reseller, resellers, servidor, servidores, servidor dedicado, servidores dedicados ">
+<meta http-equiv="keywords" content="hosting, hosting barato, hosting economico, dominio, dominios, dominios baratos, reseller, resellers, servidor, servidores, servidor dedicado, servidores dedicados" />
+<meta name="description" content="Hosting, Dominios, Resellers, Servidores Dedicados, Certificados SSL, Diseño Web y mucho mas ...">
+<meta http-equiv="description" content="hosting, dominios, resellers, servidores dedicados, certificados ssl, diseño web y mucho mas ..." />
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-color: #fafafa;
+}
+body,td,th {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 10px;
+	color: #666666;
+}
+a {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 10px;
+	color: #FFFFFF;
+}
+a:link {
+	text-decoration: none;
+	color: #666666;
+}
+a:visited {
+	text-decoration: none;
+	color: #666666;
+}
+a:hover {
+	text-decoration: none;
+	color: #CCCCCC;
+}
+a:active {
+	text-decoration: none;
+	color: #666666;
+}
+.Estilo4 {color: #666666}
+-->
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
+
+<body>
+<table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center" valign="middle"><table width="780" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td align="center" valign="middle"><p><img src="http://www.virtualhostingdigital.com/logo.png" width="400" height="150" border="0" usemap="#Map">
+            <map name="Map">
+                <area shape="rect" coords="30,41,361,117" href="http://www.virtualhostingdigital.com" alt="Acceder a la Web">
+              </map></p>
+          </td>
+        </tr>
+    </table></td>
+  </tr>
+</table>
+
+
+<map name="Map2"><area shape="rect" coords="9,10,41,29" href="mailto:info@virtualhostingdigital.com" alt="Enviar Correo">
+</map></body>
+</html>
